768ca07e4b72d74dee4311eead2d0d8c279bc21b
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-11-02 22:28:37 +0800

    update: 缩小导航文本间隔

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,25 @@ import { RouterLink, RouterView } from 'vue-router'
 
   <header class="app-header glass-nav">
     <h1 class="wrapper brand">字体字重测试</h1>
-      <nav class="header-shell" aria-label="主导航">
+      <nav class="header-shell" aria-label="导航">
         <ul class="ul">
           <li class="li">
-            <RouterLink class="nothing" active-class="active" to="/">首&nbsp&nbsp&nbsp&nbsp页</RouterLink>
+            <RouterLink class="nothing" active-class="active" to="/">首页</RouterLink>
           </li>
           <li class="li">
             <RouterLink class="nothing" active-class="active" to="/sans-serif">无衬线</RouterLink>
           </li>
           <li class="li">
-            <RouterLink class="nothing" active-class="active" to="/serif">衬&nbsp&nbsp&nbsp&nbsp线</RouterLink>
+            <RouterLink class="nothing" active-class="active" to="/serif">衬线</RouterLink>
           </li>
           <li class="li">
-            <RouterLink class="nothing" active-class="active" to="/monospace">等&nbsp&nbsp&nbsp&nbsp宽</RouterLink>
+            <RouterLink class="nothing" active-class="active" to="/monospace">等宽</RouterLink>
           </li>
           <li class="li">
-            <RouterLink class="nothing" active-class="active" to="/ja-ko">日&nbsp&nbsp&nbsp&nbsp韩</RouterLink>
+            <RouterLink class="nothing" active-class="active" to="/ja-ko">日韩</RouterLink>
           </li>
           <li class="li">
-            <RouterLink class="nothing" active-class="active" to="/character-set">编&nbsp&nbsp&nbsp&nbsp码</RouterLink>
+            <RouterLink class="nothing" active-class="active" to="/character-set">编码</RouterLink>
           </li>
         </ul>
       </nav>
