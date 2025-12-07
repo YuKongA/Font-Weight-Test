8c6ac7062b4b8d1d736bb653ea0784e72e16c6e1
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-12-07 14:31:53 +0800

    update: 改进 CJK Unified 展示

--- src/views/CharacterSetView.vue
+++ src/views/CharacterSetView.vue
@@ -1,9 +1,9 @@
 <template>
   <main>
-    <h2 class="section-title">GB2312(节选)</h2>
+    <h2 class="section-title">GB2312 (节选)</h2>
     <nav class="round">
       <nav id="sans-serif">
-        <nav class="wrap-normal">℃ ＄ ¤ ￠ ￡ ‰ § № ★ ⒛ ⑴ ＊ ＋ ， － ． ／ ０ お く ぐ け げ こト ド ナ ニ ヌ ネ ノ ハ バ パ ︵ ︶ ︹ ︺
+        <nav class="gb-block">℃ ＄ ¤ ￠ ￡ ‰ § № ★ ⒛ ⑴ ＊ ＋ ， － ． ／ ０ お く ぐ け げ こト ド ナ ニ ヌ ネ ノ ハ バ パ ︵ ︶ ︹ ︺
           ︿ ﹀
           И Й К Л М Н ㄒ ㄙ ㄚ ㄛ ┚ ┛ ├ 卮 氐 囟 胤 馗 毓 睾 鼗 亟 僖 儆 僭 僬 僦 苻 苓 茑 茚 茆 茔 茕 掼 揲 揸 揠 揿 揄 揞 揎 唣 猬 猸 猱 獐 獍 獗 獠 獬 阋
           阚 滏
@@ -11,10 +11,10 @@
       </nav>
     </nav>
 
-    <h2 class="section-title">GBK(节选)</h2>
+    <h2 class="section-title">GBK (节选)</h2>
     <nav class="round">
       <nav id="sans-serif">
-        <nav class="wrap-normal">〇 〡 〢 〣 〤 〥 〦 〧 〨 〩 ㊣ ㎎ ㎏ ㎜ ㎝ ㎞ ㎡ ﹗ ﹞ ﹟ ﹠ ﹡ ˙ ― ℅ ℉ ↙∕ ∟ ∣ ╬ ╭ ╯ ╱ ╲ ╳ ▁ ▂
+        <nav class="gb-block">〇 〡 〢 〣 〤 〥 〦 〧 〨 〩 ㊣ ㎎ ㎏ ㎜ ㎝ ㎞ ㎡ ﹗ ﹞ ﹟ ﹠ ﹡ ˙ ― ℅ ℉ ↙∕ ∟ ∣ ╬ ╭ ╯ ╱ ╲ ╳ ▁ ▂
           ▃ ▄▕
           ▼ ╜ ╫ 丏 丒 丗 丟 丠 両 丣 喩 喪 喫 喬 喭 單 喯 尷 屃 屄 屆 屇 屌 屍 屒 屓 屔 屖 屗 屘 撘 撚 撛 撜 撝 撟 撠 撡 撢 撣 撥 撦 撧 撨 柪 柫 柭 柮 柲 柵 柶 柷
           柸 柹
@@ -22,41 +22,58 @@
       </nav>
     </nav>
 
-    <h2 class="section-title">GB18030-2000(节选)</h2>
+    <h2 class="section-title">GB18030-2000 (节选)</h2>
     <nav class="round">
       <nav id="sans-serif">
-        <nav class="wrap-normal">Ä Æ Å À Á Â Ã Ç È É Ê Ë Ð Ì Í Î Ï Ö Ø Ò Ó Ô Õ Ñ Ù Ú Û Ü Ý Þ ä æ å â ã ç ë ð
+        <nav class="gb-block">Ä Æ Å À Á Â Ã Ç È É Ê Ë Ð Ì Í Î Ï Ö Ø Ò Ó Ô Õ Ñ Ù Ú Û Ü Ý Þ ä æ å â ã ç ë ð
           î ï
           ö ø ô õ ñ û ý þ 㑇 㑊 㕮 㘎 㙍 㙘 㙦 㛃 㛚 㛹 㟃 㠇 㠓 㤘 㥄 㧐 㧑 㧟 㫰 㬊 㬎 㬚 㭎 㭕 㮾 㰀 㳇 㳘 㳚 㴔 㵐 㶲 㸆 㸌 㺄 㻬 㽏 㿠 䁖 䂮 䃅 䃎 䅟
           䌹 䎃 䎖 䏝 䏡 䏲 䐃 䓖 䓛 䓨 䓫 䓬 䗖 䗛 䗪 䗴 䜣 䝙 䢺 䢼 䣘 䥽 䦃 䲟 䲠 䲢 䴓 䴔 䴕 䴖 䴗 䴘 䴙 䶮</nav>
       </nav>
     </nav>
 
-    <h2 class="section-title">附. CJK (节选)</h2>
+    <h2 class="section-title">CJK Unified (节选)</h2>
     <nav class="round">
       <nav id="sans-serif">
-        <nav class="wrap-normal">
-          <span class="accent">扩展 A 区: </span>㐀 㐁 㐂 㐃 㐄 㐅 㐆 㐇 㐈 㐉 㐊 㐋 㐌 㐍 㐎 㐏<br>
-          <span class="accent">扩展 B 区: </span>𠀀 𠀁 𠀂 𠀃 𠀄 𠀅 𠀆 𠀇 𠀈 𠀉 𠀊 𠀋 𠀌 𠀍 𠀎 𠀏<br>
-          <span class="accent">2003 新增: </span>龦 龧 龨 龩 龪 龫 龬 龭 龮 龯 龰 龱 龲 龳 龴 龵<br>
-          <span class="accent">扩展 C 区: </span>𪜀 𪜁 𪜂 𪜃 𪜄 𪜅 𪜆 𪜇 𪜈 𪜉 𪜊 𪜋 𪜌 𪜍 𪜎 𪜏<br>
-          <span class="accent">扩展 D 区: </span>𫝀 𫝁 𫝂 𫝃 𫝄 𫝅 𫝆 𫝇 𫝈 𫝉 𫝊 𫝋 𫝌 𫝍 𫝎 𫝏<br>
-          <span class="accent">2012 新增: </span>鿌<br>
-          <span class="accent">扩展 E 区: </span>𫠠 𫠡 𫠢 𫠣 𫠤 𫠥 𫠦 𫠧 𫠨 𫠩 𫠪 𫠫 𫠬 𫠭 𫠮 𫠯<br>
-          <span class="accent">2015 "急用汉字": </span>鿍 鿎 鿏 䲤 鿑 鿒 鿓 鿔 鿕<br>
-          <span class="accent">扩展 F 区: </span>𬺰 𬺱 𬺲 𬺳 𬺴 𬺵 𬺶 𬺷 𬺸 𬺹 𬺺 𬺻 𬺼 𬺽 𬺾 𬺿<br>
-          <span class="accent">2017 新增: </span>鿥 鿦 鿧 鿨 鿩 鿪 鿖 鿗 鿙 鿚 鿜 鿠<br>
-          <span class="accent">2018 新增: </span>鿫 鿬 鿭 鿮 鿯<br>
-          <span class="accent">扩展 G 区: </span>𰀀 𰀁 𰀂 𰀃 𰀄 𰀅 𰀆 𰀇 𰀈 𰀉 𰀊 𰀋 𰀌 𰀍 𰀎 𰀏<br>
-          <span class="accent">2021 "急用科学与技术用字": </span>鿰 鿱 鿲 鿳 鿴 鿵 鿶 鿷 鿸 鿹 鿺 鿻 鿼<br>
-          <span class="accent">2021 "10 个需分离的汉字": </span>䶶 䶷 䶸 䶹 䶺 䶻 䶼 䶽 䶾 䶿<br>
-          <span class="accent">2021 "昆曲工尺谱用字": </span>𪛗 𪛘 𪛙 𪛚 𪛛 𪛜 𪛝<br>
-          <span class="accent">2021 新增: </span>鿽 鿾 鿿 𪛞 𪛟 𫜵 𫜶 𫜷 𫜸<br>
-          <span class="accent">扩展 H 区: </span>𱍐 𱍑 𱍒 𱍓 𱍔 𱍕 𱍖 𱍗 𱍘 𱍙 𱍚 𱍛 𱍜 𱍝 𱍞 𱍟<br>
-          <span class="accent">2022 新增: </span>𫜹<br>
-          <span class="accent">扩展 I 区: </span>𮯰 𮯱 𮯲 𮯳 𮯴 𮯵 𮯶 𮯷 𮯸 𮯹 𮯺 𮯻 𮯼 𮯽 𮯾 𮯿<br>
-          <span class="accent">扩展 J 区: </span>𲎰 𲎱 𲎲 𲎳 𲎴 𲎵 𲎶 𲎷 𲎸 𲎹 𲎺 𲎻 𲎼 𲎽 𲎾 𲎿<br>
-        </nav>
+        <section class="cjk-columns">
+          <div class="cjk-item"><span class="accent">扩展 A 区:</span><span class="cjk-text">㐀 㐁 㐂 㐃 㐄 㐅 㐆 㐇 㐈 㐉 㐊 㐋 㐌 㐍 㐎
+              㐏</span></div>
+          <div class="cjk-item"><span class="accent">扩展 B 区:</span><span class="cjk-text">𠀀 𠀁 𠀂 𠀃 𠀄 𠀅 𠀆 𠀇 𠀈 𠀉
+              𠀊 𠀋 𠀌 𠀍 𠀎 𠀏</span></div>
+          <div class="cjk-item"><span class="accent">2003 新增:</span><span class="cjk-text">龦 龧 龨 龩 龪 龫 龬 龭 龮 龯 龰 龱 龲 龳 龴
+              龵</span></div>
+          <div class="cjk-item"><span class="accent">扩展 C 区:</span><span class="cjk-text">𪜀 𪜁 𪜂 𪜃 𪜄 𪜅 𪜆 𪜇 𪜈 𪜉
+              𪜊 𪜋 𪜌 𪜍 𪜎 𪜏</span></div>
+          <div class="cjk-item"><span class="accent">扩展 D 区:</span><span class="cjk-text">𫝀 𫝁 𫝂 𫝃 𫝄 𫝅 𫝆 𫝇 𫝈 𫝉
+              𫝊 𫝋 𫝌 𫝍 𫝎 𫝏</span></div>
+          <div class="cjk-item"><span class="accent">2012 新增:</span><span class="cjk-text">鿌</span></div>
+          <div class="cjk-item"><span class="accent">扩展 E 区:</span><span class="cjk-text">𫠠 𫠡 𫠢 𫠣 𫠤 𫠥 𫠦 𫠧 𫠨 𫠩
+              𫠪 𫠫 𫠬 𫠭 𫠮 𫠯</span></div>
+          <div class="cjk-item"><span class="accent">2015 "急用汉字":</span><span class="cjk-text">鿍 鿎 鿏 䲤 鿑 鿒 鿓 鿔 鿕</span>
+          </div>
+          <div class="cjk-item"><span class="accent">扩展 F 区:</span><span class="cjk-text">𬺰 𬺱 𬺲 𬺳 𬺴 𬺵 𬺶 𬺷 𬺸 𬺹
+              𬺺 𬺻 𬺼 𬺽 𬺾 𬺿</span></div>
+          <div class="cjk-item"><span class="accent">2017 新增:</span><span class="cjk-text">鿥 鿦 鿧 鿨 鿩 鿪 鿖 鿗 鿙 鿚 鿜
+              鿠</span></div>
+          <div class="cjk-item"><span class="accent">2018 新增:</span><span class="cjk-text">鿫 鿬 鿭 鿮 鿯</span></div>
+          <div class="cjk-item"><span class="accent">扩展 G 区:</span><span class="cjk-text">𰀀 𰀁 𰀂 𰀃 𰀄 𰀅 𰀆 𰀇 𰀈 𰀉
+              𰀊 𰀋 𰀌 𰀍 𰀎 𰀏</span></div>
+          <div class="cjk-item"><span class="accent">2021 "急用科学与技术用字":</span><span class="cjk-text">鿰 鿱 鿲 鿳 鿴 鿵 鿶 鿷 鿸 鿹
+              鿺 鿻 鿼</span></div>
+          <div class="cjk-item"><span class="accent">2021 "10 个需分离的汉字":</span><span class="cjk-text">䶶 䶷 䶸 䶹 䶺 䶻 䶼 䶽 䶾
+              䶿</span></div>
+          <div class="cjk-item"><span class="accent">2021 "昆曲工尺谱用字":</span><span class="cjk-text">𪛗 𪛘 𪛙 𪛚 𪛛 𪛜
+              𪛝</span></div>
+          <div class="cjk-item"><span class="accent">2021 新增:</span><span class="cjk-text">鿽 鿾 鿿 𪛞 𪛟 𫜵 𫜶 𫜷
+              𫜸</span></div>
+          <div class="cjk-item"><span class="accent">扩展 H 区:</span><span class="cjk-text">𱍐 𱍑 𱍒 𱍓 𱍔 𱍕 𱍖 𱍗 𱍘 𱍙
+              𱍚 𱍛 𱍜 𱍝 𱍞 𱍟</span></div>
+          <div class="cjk-item"><span class="accent">2022 新增:</span><span class="cjk-text">𫜹</span></div>
+          <div class="cjk-item"><span class="accent">扩展 I 区:</span><span class="cjk-text">𮯰 𮯱 𮯲 𮯳 𮯴 𮯵 𮯶 𮯷 𮯸 𮯹
+              𮯺 𮯻 𮯼 𮯽 𮯾 𮯿</span></div>
+          <div class="cjk-item"><span class="accent">扩展 J 区:</span><span class="cjk-text">𲎰 𲎱 𲎲 𲎳 𲎴 𲎵 𲎶 𲎷 𲎸 𲎹
+              𲎺 𲎻 𲎼 𲎽 𲎾 𲎿</span></div>
+        </section>
       </nav>
     </nav>
   </main>
